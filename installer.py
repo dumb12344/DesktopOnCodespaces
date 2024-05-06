868b503d523b0fe3e2baaa8e2318bb1e29b7f670
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2024-05-06 10:22:54 -0700

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,45 @@
+import os
+import json
+
+#JSON export
+def savejson(json):
+    with open('options.json', 'w') as f:
+        f.write(str(json).replace("'", '"').replace("True", "true").replace("False", "false"))
+def environmentconversion(env):
+  if env = "0":
+    return "KDE Plasma (Heavy)"
+  elif env = "1":
+    return "XFCE4 (Lightweight)"
+  elif env = "2":
+    return "I3 (Very Lightweight)"
+  elif env = "3":
+    return "GNOME 42 (Very Heavy)"
+  elif env = "4":
+    return "Cinnamon"
+  elif env = "5":
+    return "LXQT"
+  else:
+    return env
+defaultjson = {"defaultapps": [], "programming": [], "apps": [], "enablekvm": true, "DE": "KDE Plasma (Heavy)"}
+data = defaultjson
+wine = input("Do you want to install wine? (Y/n)")
+brave = input("Do you want to install brave? (Y/n)")
+xarchiver = input("Do you want to install Xarchiver? (Y/n)")
+if not wine = "n":
+  data["defaultapps"].append(0)
+if not brave = "n":
+  data["defaultapps"].append(1)
+if not xarchiver = "n":
+  data["defaultapps"].append(2)
+environment = input("""
+Which environment do you want:
+KDE Plasma (Heavy) : 0
+XFCE4 (Lightweight) : 1
+I3 (Very Lightweight) : 2
+GNOME 42 (Very Heavy) : 3
+Cinnamon : 4
+LXQT : 5
+""")
+environment = environmentconversion(environment)
+data["DE"] = environment
+savejson(data)
