cf3efa724e42179dd47d164c17df7f18afa5bb95
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2024-05-06 11:22:24 -0700

    Create installalpine.sh

---
+++ installalpine.sh
@@ -0,0 +1,22 @@
+git clone https://github.com/dumb12344/DesktopOnCodespaces
+cd DesktopOnCodespaces
+#pip install textual
+sleep 2
+python3 installer.py
+docker build -t desktoponcodespaces . --no-cache
+cd ..
+
+sudo apk update
+sudo apk add jq
+
+mkdir Save
+cp -r DesktopOnCodespaces/root/config/* Save
+
+json_file="DesktopOnCodespaces/options.json"
+if jq ".enablekvm" "$json_file" | grep -q true; then
+    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
+else
+    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
+fi
+clear
+echo "INSTALL FINISHED! Check Port Tab"
